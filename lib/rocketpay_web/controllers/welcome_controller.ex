760ddd32fed56777b2c586b46224e7b03c845a41
nurbanetz <nurbanetz@keybiz.it>
2021-02-24 00:28:50 +0100

    init elixir

---
+++ lib/rocketpay_web/controllers/welcome_controller.ex
@@ -0,0 +1,24 @@
+defmodule RocketpayWeb.WelcomeController do
+  use RocketpayWeb, :controller
+
+  alias Rocketpay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+      |> Numbers.sum_from_file()
+      |> handle_response(conn)
+    text(conn, "Welcome to the Rocketpay API")
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+     |> put_status(:ok)
+     |> json(%{message: "welcome to rocketpay api, Here is your number #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+     |> put_status(:bad_request)
+     |> json(reason)
+  end
+end
