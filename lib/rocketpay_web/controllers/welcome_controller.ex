f970a4d61faf03f0a6fa685dd017fdef8cfa8489
Newton Urbanetz <new.urbanetz@gmail.com>
2021-02-26 15:02:38 +0100

    feat: added router and view users

--- lib/rocketpay_web/controllers/welcome_controller.ex
+++ lib/rocketpay_web/controllers/welcome_controller.ex
@@ -5,20 +5,21 @@ defmodule RocketpayWeb.WelcomeController do
 
   def index(conn, %{"filename" => filename}) do
     filename
-      |> Numbers.sum_from_file()
-      |> handle_response(conn)
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+
     text(conn, "Welcome to the Rocketpay API")
   end
 
   defp handle_response({:ok, %{result: result}}, conn) do
     conn
-     |> put_status(:ok)
-     |> json(%{message: "welcome to rocketpay api, Here is your number #{result}"})
+    |> put_status(:ok)
+    |> json(%{message: "welcome to rocketpay api, Here is your number #{result}"})
   end
 
   defp handle_response({:error, reason}, conn) do
     conn
-     |> put_status(:bad_request)
-     |> json(reason)
+    |> put_status(:bad_request)
+    |> json(reason)
   end
 end
