f7289ab047fc5df9dcd14e9a06224b75aa827d6d
Newton Urbanetz <new.urbanetz@gmail.com>
2021-02-28 00:13:00 +0100

    feat: nlw#4

---
+++ test/rocketpay/users/create_test.exs
@@ -0,0 +1,40 @@
+defmodule Rocketpay.Users.CreateTest do
+  use Rocketpay.DataCase
+
+  alias Rocketpay.User
+  alias Rocketpay.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, returns an user" do
+      params = %{
+        name: "John",
+        password: "123456",
+        nickname: "john",
+        email: "john@email.com",
+        age: 18
+      }
+
+      {:ok, %User{id: user_id}} = Create.call(params)
+
+      user = Repo.get(User, user_id)
+
+      assert %User{name: "John", age: 18, id: ^user_id} = user
+    end
+
+    test "when there are invalid params, returns an error" do
+      params = %{
+        name: "John",
+        password: "123456",
+        nickname: "john",
+        email: "john@email.com",
+        age: 15
+      }
+
+      {:error, changeset} = Create.call(params)
+
+      expected_response = %{age: ["must be greater than or equal to 18"]}
+
+      assert expected_response == errors_on(changeset)
+    end
+  end
+end
---
+++ test/rocketpay_web/controllers/accounts_controller_test.exs
@@ -0,0 +1,50 @@
+defmodule RocketpayWeb.AccountsControllerTest do
+  use RocketpayWeb.ConnCase
+
+  alias Rocketpay.{Account, User}
+
+  describe "deposit/2" do
+    setup %{conn: conn} do
+      params = %{
+        name: "John",
+        password: "123456",
+        nickname: "john",
+        email: "john@email.com",
+        age: 18
+      }
+
+      {:ok, %User{account: %Account{id: account_id}}} = Rocketpay.create_user(params)
+
+      conn = put_req_header(conn, "authorization", "Basic YWRtaW46YWRtaW4=")
+
+      {:ok, conn: conn, account_id: account_id}
+    end
+
+    test "when all params are valid, make the deposit", %{conn: conn, account_id: account_id} do
+      params = %{"value" => "60.00"}
+
+      response =
+        conn
+        |> post(Routes.accounts_path(conn, :deposit, account_id, params))
+        |> json_response(:ok)
+
+      assert %{
+               "account" => %{"balance" => "60.00", "id" => _id},
+               "message" => "Balance changed successfully"
+             } = response
+    end
+
+    test "when there are invalid params, returns an error", %{conn: conn, account_id: account_id} do
+      params = %{"value" => "aaa"}
+
+      response =
+        conn
+        |> post(Routes.accounts_path(conn, :deposit, account_id, params))
+        |> json_response(:bad_request)
+
+      expected_response = %{"message" => "Invalid operation value"}
+
+      assert response == expected_response
+    end
+  end
+end
